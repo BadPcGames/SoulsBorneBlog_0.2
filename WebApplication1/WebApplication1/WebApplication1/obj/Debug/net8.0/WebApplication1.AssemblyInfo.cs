eac9deb851f8f4a010beb015638f75bef5cd6246
BadPcGames <v.y.shumsjkyy@student.khai.edu>
2025-04-24 15:58:47 +0300

    added design for home page

--- WebApplication1/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4f86b6dd980a151fb7439cd23a391ded6e2757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5e39f4d8cc46e17cca778ec527235fba8d736b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
